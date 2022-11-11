441186a3a06ff8a91f4c3d9ea5ab4da3c1238894
Mark-progg <mark.mishutin@yandex.ru>
2022-11-11 14:11:36 +0300

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int creditCardNumber = 0xCC275;
+        long mobileNumber = 89537772211L;
+        int twoLastDigits = 0b1011;
+        int fourLastDigits = 04243;
+        int magicNumber = (13 - 1) % 26 + 1;
+        char engSymbol = (char) (magicNumber + 64);
+
+        System.out.println(creditCardNumber);
+        System.out.println(mobileNumber);
+        System.out.println(twoLastDigits);
+        System.out.println(fourLastDigits);
+        System.out.println(magicNumber);
+        System.out.println(engSymbol);
     }
 }
